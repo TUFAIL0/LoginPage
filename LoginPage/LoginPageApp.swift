9ecf87e25b1d56f818657871f0ce0d40e9bb21ff
Tufail001 <sta07866@gmail.com>
2023-10-13 22:39:25 +0530

    First Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by Tufail Ahmad on 13/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
